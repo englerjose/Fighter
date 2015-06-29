ae21c98d11d823f092c56228bda991539a1c9bce
englerjose <englerjosegithub@gmail.com>
2015-06-29 17:01:40 +1200

    Add black border to healthbars

--- overlay.rb
+++ overlay.rb
@@ -42,6 +42,7 @@ module Fighter
       x = @player.side == :left ? 20 : @window.width - width - 20
       y = 20
       color = Gosu::Color::RED
+      @window.draw_quad(x-5, y-5, Gosu::Color::BLACK, x+5+width, y-5, Gosu::Color::BLACK, x+5+width, 2*y+5, Gosu::Color::BLACK, x-5, 2*y+5, Gosu::Color::BLACK, 1)
       @window.draw_quad(x, y, color, x + width, y, color, x+width, 2*y, color, x, 2*y, color, 1)
     end
   end
